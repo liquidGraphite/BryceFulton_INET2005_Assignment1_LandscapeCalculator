daa60879d3be9b6202bf3318d306e79daaf4c4d7
Bryce <bryce.fulton2000@outlook.com>
2024-09-30 15:16:55 -0300

    initial commit for landscape calculator

---
+++ src/main/java/com/example/landscapecalculator/LandscapeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.landscapecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandscapeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandscapeCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/landscapecalculator/LandscapeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.landscapecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandscapeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
